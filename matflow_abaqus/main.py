54c53637627672fe4d2fada4972190c993490a49
maria-yankova <maria.yankova@manchester.ac.uk>
2020-12-15 10:23:49 +0000

    Add generate parts & steps, write input file

--- matflow_abaqus/main.py
+++ matflow_abaqus/main.py
@@ -1,6 +1,9 @@
 '`matflow_abaqus.main.py`'
 
-from abaqus_parse import materials 
+from abaqus_parse import materials
+from abaqus_parse.parts import generate_compact_tension_specimen_parts
+from abaqus_parse.steps import generate_compact_tension_specimen_steps
+from abaqus_parse.writers import write_inp
 
 from matflow_abaqus import (
     input_mapper,
@@ -19,3 +22,25 @@ def generate_material_models(materials_list):
         'material_models': mat_mods
     }
     return out
+
+
+@func_mapper(task='generate_specimen_parts', method='compact_tension_fracture')
+def generate_parts(dimension, mesh_definition,
+                     elem_type, size_type, fraction, specimen_material):
+    specimen_parts = generate_compact_tension_specimen_parts(dimension, mesh_definition, elem_type, size_type, fraction, specimen_material)
+    out = {
+        'specimen_parts': specimen_parts
+    }
+    return out
+
+@func_mapper(task='generate_steps', method='compact_tension_steps')
+def generate_steps(applied_displacement, number_contours):
+    compact_tension_steps = generate_compact_tension_specimen_steps(applied_displacement, number_contours)
+    out = {
+        'steps': compact_tension_steps
+    }
+    return out
+
+@input_mapper(input_file='inputs.inp', task='simulate_deformation', method='FE')
+def write_inputs_file(path, material_models, specimen_parts, steps):
+    write_inp(path, material_models, specimen_parts, steps)
