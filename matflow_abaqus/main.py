7528fe385c5c7bc9bac270c08993e7c49298948d
tjailin <72205930+tjailin@users.noreply.github.com>
2021-02-10 08:43:45 +0100

    New functions for the MK model

--- matflow_abaqus/main.py
+++ matflow_abaqus/main.py
@@ -5,6 +5,12 @@ from abaqus_parse.parts import generate_compact_tension_specimen_parts
 from abaqus_parse.steps import generate_compact_tension_specimen_steps
 from abaqus_parse.writers import write_inp
 
+from abaqus_parse.Generate_MK_mesh import Generate_MK_mesh
+from abaqus_parse.Generate_FE_sample import Generate_FE_sample
+from abaqus_parse.Generate_FE_features import Generate_FE_features
+from abaqus_parse.Save_model_response import Save_model_response
+
+
 from matflow_abaqus import (
     input_mapper,
     output_mapper,
@@ -13,6 +19,7 @@ from matflow_abaqus import (
     func_mapper,
     software_versions,
 )
+
 
 # tells Matflow this function satisfies the requirements of the task
 @func_mapper(task='generate_material_models', method='default')
@@ -48,3 +55,38 @@ def write_inputs_file(path, material_models, specimen_parts, steps):
 @cli_format_mapper(input_name="memory", task="simulate_deformation", method="FE")
 def memory_formatter(memory):
     return f'memory={memory.replace(" ", "")}'
+	
+	
+###################################################################################
+###################################################################################
+
+	
+@func_mapper(task='generate_Sample', method='default')
+def Generate_sample(Sample_size, Inhomogeneity_factor, L_groove, L_slope, Material_angle, Groove_angle, Elastic_modulus, Poisson_ratio, Density, Barlat, Path_plastic_table):
+    Sample_input = Generate_FE_sample(Sample_size, Inhomogeneity_factor, L_groove, L_slope, Material_angle, Groove_angle, Elastic_modulus, Poisson_ratio, Density, Barlat, Path_plastic_table)
+    out = {
+        'Sample_input_data': Sample_input
+    }
+    return out
+    
+        
+@func_mapper(task='generate_FE_features', method='default')
+def FE_features(mesh_size, bulk_parameters, elem_type, Strain_rate, total_time, Displacment_BC, Step_time):
+    FE_input = Generate_FE_features(mesh_size, bulk_parameters, elem_type, Strain_rate, total_time, Displacment_BC, Step_time)
+    out = {
+        'FE_input_data': FE_input
+    }
+    return out
+
+@input_mapper(input_file='inputs.inp', task='simulate_deformation', method='FE')
+def write_MK_inputs_file(path, Sample_input_data, FE_input_data):
+    Generate_MK_mesh(path, Sample_input_data, FE_input_data)
+    
+    
+@output_mapper(output_name="model_response", task='simulate_deformation', method='FE')
+def Generate_model_response(path):
+    model_response = Save_model_response(path)
+    out = {
+        'model_response': model_response
+    }
+    return out
